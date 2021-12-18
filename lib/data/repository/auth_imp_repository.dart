341a89988ff35d72635bebb21dfa22d07a574c61
Alvaro Prado <alvarob.prado@hotmail.com>
2021-12-18 16:25:40 -0400

    Add initial imp repositories

---
+++ lib/data/repository/auth_imp_repository.dart
@@ -0,0 +1,3 @@
+import 'package:domain/data_repository/auth_data_repository.dart';
+
+class AuthImpRepository implements AuthDataRepository {}
---
+++ lib/data/repository/courses_imp_repository.dart
@@ -0,0 +1,3 @@
+import 'package:domain/data_repository/courses_data_repository.dart';
+
+class CoursesImpRepository implements CoursesDataRepository {}
