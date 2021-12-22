843561233ed0a72ad92aa563bf94ab35b778fd21
Alvaro Prado <alvarob.prado@hotmail.com>
2021-12-22 16:07:39 -0400

    Add initial auth repository

--- lib/app/data/repository/auth_imp_repository.dart
+++ lib/app/data/repository/auth_imp_repository.dart
@@ -1,3 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:faeng_courses/app/domain/data_repository/auth_data_repository.dart';
 
-class AuthImpRepository implements AuthDataRepository {}
+class AuthImpRepository implements AuthDataRepository {
+  AuthImpRepository({
+    required FirebaseAuth authProvider,
+  }) : _authProvider = authProvider;
+
+  final FirebaseAuth _authProvider;
+
+  @override
+  Stream<User?> get authStateChanges => _authProvider.authStateChanges();
+
+  @override
+  User? getCurrentUser() => _authProvider.currentUser;
+
+  @override
+  Future<void> signInAnonmously() async {
+    await _authProvider.signInAnonymously();
+  }
+
+  @override
+  Future<void> signOut() async {
+    await _authProvider.signOut();
+    await _authProvider.signInAnonymously();
+  }
+
+  @override
+  Future<User?> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    final userCredential = await _authProvider.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    return userCredential.user;
+  }
+}
