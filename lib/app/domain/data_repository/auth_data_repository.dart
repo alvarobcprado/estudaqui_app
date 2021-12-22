93751c607a0b3943f415d62017ae7fef5a9ce909
Alvaro Prado <alvarob.prado@hotmail.com>
2021-12-22 16:07:39 -0400

    Add initial auth repository

--- lib/app/domain/data_repository/auth_data_repository.dart
+++ lib/app/domain/data_repository/auth_data_repository.dart
@@ -1,3 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 abstract class AuthDataRepository {
-  const AuthDataRepository();
+  Stream<User?> get authStateChanges;
+
+  User? getCurrentUser();
+
+  Future<void> signInAnonmously();
+
+  Future<void> signOut();
+
+  Future<User?> signInWithEmailAndPassword(String email, String password);
 }
