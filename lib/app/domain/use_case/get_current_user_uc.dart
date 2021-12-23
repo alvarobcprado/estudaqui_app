528db4170d5d0d49fb259395e5bbd3638b0ea048
Alvaro Prado <alvarob.prado@hotmail.com>
2021-12-22 21:56:09 -0400

    Create GetCurrentUserUC and its unit test

--- lib/app/domain/use_case/get_current_user_uc.dart
+++ lib/app/domain/use_case/get_current_user_uc.dart
@@ -1,12 +1,11 @@
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:faeng_courses/app/domain/data_repository/auth_data_repository.dart';
 import 'package:faeng_courses/app/domain/use_case/use_case.dart';
 import 'package:faeng_courses/core/error/failures.dart';
 
-class GetCurrentUserUC extends UseCase<GetCurrentUserParams, User> {
+class GetCurrentUserUC extends UseCase<NoParams, User> {
   GetCurrentUserUC({
     required AuthDataRepository repository,
   }) : _repository = repository;
@@ -15,13 +14,7 @@ class GetCurrentUserUC extends UseCase<GetCurrentUserParams, User> {
 
   @override
   Future<Either<Failure, User>> call({
-    required GetCurrentUserParams params,
-  }) async {
-    return _repository.getCurrentUser();
-  }
-}
-
-class GetCurrentUserParams extends Equatable {
-  @override
-  List<Object?> get props => throw UnimplementedError();
+    required NoParams params,
+  }) async =>
+      _repository.getCurrentUser();
 }
