d577e47c871c9d5055d239c487b17b13f7c7b002
Alvaro Prado <alvarob.prado@hotmail.com>
2021-12-27 16:56:34 -0400

    Create SigninAnonmouslyUC and its unit test

--- lib/app/domain/use_case/signin_anonmously_uc.dart
+++ lib/app/domain/use_case/signin_anonmously_uc.dart
@@ -1,20 +1,18 @@
 import 'package:dartz/dartz.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:faeng_courses/app/domain/data_repository/auth_data_repository.dart';
 import 'package:faeng_courses/app/domain/use_case/use_case.dart';
 import 'package:faeng_courses/core/error/failures.dart';
 
-class GetCurrentUserUC extends UseCase<NoParams, User> {
-  GetCurrentUserUC({
+class SigninAnonmouslyUC extends UseCase<NoParams, void> {
+  SigninAnonmouslyUC({
     required AuthDataRepository authRepository,
   }) : _authRepository = authRepository;
 
   final AuthDataRepository _authRepository;
 
   @override
-  Future<Either<Failure, User>> call({
-    required NoParams params,
-  }) async =>
-      _authRepository.getCurrentUser();
+  Future<Either<Failure, void>> call({required params}) async {
+    return _authRepository.signInAnonmously();
+  }
 }
