a57d84080ac0ef828d0d3900cb576e704c0d5ab8
Alvaro Prado <alvarob.prado@hotmail.com>
2021-12-28 19:01:06 -0400

    Create SignupWithEmailAndPassword and its unit test

---
+++ lib/app/domain/use_case/signup_with_email_and_password_uc.dart
@@ -0,0 +1,36 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:faeng_courses/app/domain/data_repository/auth_data_repository.dart';
+import 'package:faeng_courses/app/domain/use_case/use_case.dart';
+import 'package:faeng_courses/core/error/failures.dart';
+
+class SignupWithEmailAndPasswordUC
+    extends UseCase<SignupWithEmailAndPasswordParams, User> {
+  SignupWithEmailAndPasswordUC({
+    required AuthDataRepository authDataRepository,
+  }) : _authDataRepository = authDataRepository;
+
+  final AuthDataRepository _authDataRepository;
+
+  @override
+  Future<Either<Failure, User>> call({required params}) async {
+    return _authDataRepository.signUpWithEmailAndPassword(
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+class SignupWithEmailAndPasswordParams extends Equatable {
+  const SignupWithEmailAndPasswordParams({
+    required this.email,
+    required this.password,
+  });
+
+  final String email;
+  final String password;
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
