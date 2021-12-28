ae3f46c964d3b39c14d0933101da941c28ab0df1
Alvaro Prado <alvarob.prado@hotmail.com>
2021-12-28 19:01:06 -0400

    Create SignupWithEmailAndPassword and its unit test

---
+++ test/app/domain/use_case/signup_with_email_and_password_uc_test.dart
@@ -0,0 +1,60 @@
+import 'package:dartz/dartz.dart';
+import 'package:faeng_courses/app/domain/data_repository/auth_data_repository.dart';
+import 'package:faeng_courses/app/domain/use_case/signup_with_email_and_password_uc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'signup_with_email_and_password_uc_test.mocks.dart';
+
+@GenerateMocks([AuthDataRepository, User])
+void main() {
+  late final MockAuthDataRepository mockRepository;
+  MockUser? mockUser;
+  late final SignupWithEmailAndPasswordUC useCase;
+  const _email = 'email@email.com';
+  const _password = 'password';
+
+  setUp(
+    () {
+      mockRepository = MockAuthDataRepository();
+      useCase = SignupWithEmailAndPasswordUC(
+        authDataRepository: mockRepository,
+      );
+    },
+  );
+
+  test(
+    'should signup a user with email and password',
+    () async {
+      when(
+        mockRepository.signUpWithEmailAndPassword(
+          email: argThat(isNotNull, named: 'email'),
+          password: argThat(isNotNull, named: 'password'),
+        ),
+      ).thenAnswer(
+        (_) async {
+          mockUser = MockUser();
+          return Right(mockUser!);
+        },
+      );
+
+      expect(mockUser, isNull);
+      final result = await useCase.call(
+        params: const SignupWithEmailAndPasswordParams(
+          email: _email,
+          password: _password,
+        ),
+      );
+      expect(result, Right(mockUser));
+      verify(
+        mockRepository.signUpWithEmailAndPassword(
+          email: _email,
+          password: _password,
+        ),
+      ).called(1);
+      verifyNoMoreInteractions(mockRepository);
+    },
+  );
+}
