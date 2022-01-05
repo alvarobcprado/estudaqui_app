57dc9f722a2acccbc738c321fc7c52541def2282
Alvaro Prado <alvarob.prado@hotmail.com>
2022-01-05 11:44:32 -0400

    Update location and add comments for responsive and constants

--- lib/app/presentation/common/utils/constants.dart
+++ lib/app/presentation/common/utils/constants.dart
@@ -1,13 +1,35 @@
+/// Padding 8.0
 const kSmallPadding = 8.0;
+
+/// Padding 16.0
 const kMediumPadding = 16.0;
+
+/// Padding 24.0
 const kLargePadding = 24.0;
+
+/// Padding 32.0
 const kXLargePadding = 32.0;
+
+/// Padding 48.0
 const kXXLargePadding = 48.0;
+
+/// Padding 56.0
 const kXXXLargePadding = 56.0;
 
+/// Spacer 8.0
 const kSmallSpacer = 8.0;
+
+/// Spacer 16.0
 const kMediumSpacer = 16.0;
+
+/// Spacer 24.0
 const kLargeSpacer = 24.0;
+
+/// Spacer 32.0
 const kXLargeSpacer = 32.0;
+
+/// Spacer 48.0
 const kXXLargeSpacer = 48.0;
+
+/// Spacer 56.0
 const kXXXLargeSpacer = 56.0;
