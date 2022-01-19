abbcaf6d1d701c81565315178c648503253fef26
Alvaro Prado <alvarob.prado@hotmail.com>
2022-01-18 21:21:28 -0400

    Create courses repository contracts

--- lib/app/domain/data_repository/courses_data_repository.dart
+++ lib/app/domain/data_repository/courses_data_repository.dart
@@ -1,3 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:faeng_courses/app/domain/entity/course.dart';
+import 'package:faeng_courses/app/domain/entity/course_module.dart';
+import 'package:faeng_courses/core/error/failures.dart';
+
 abstract class CoursesDataRepository {
   const CoursesDataRepository();
+
+  Future<Either<Failure, List<Course>>> fetchCourses();
+
+  Future<Either<Failure, Course>> getCourseById(String courseId);
+
+  Future<Either<Failure, List<CourseModule>>> fetchCourseModules(
+    String courseId,
+  );
 }
