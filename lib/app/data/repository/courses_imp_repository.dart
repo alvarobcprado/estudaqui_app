6080b034fc06459ce64a9abf73bfde5013e96d68
Alvaro Prado <alvarob.prado@hotmail.com>
2022-01-18 21:21:47 -0400

    Create courses repository implementation and its tests

--- lib/app/data/repository/courses_imp_repository.dart
+++ lib/app/data/repository/courses_imp_repository.dart
@@ -1,3 +1,66 @@
+import 'package:dartz/dartz.dart';
+import 'package:faeng_courses/app/data/remote/data_source/courses_rds.dart';
 import 'package:faeng_courses/app/domain/data_repository/courses_data_repository.dart';
+import 'package:faeng_courses/core/error/course_failure.dart';
+import 'package:faeng_courses/core/error/failures.dart';
+import 'package:faeng_courses/app/domain/entity/course_module.dart';
+import 'package:faeng_courses/app/domain/entity/course.dart';
 
-class CoursesImpRepository implements CoursesDataRepository {}
+class CoursesImpRepository implements CoursesDataRepository {
+  CoursesImpRepository({
+    required CoursesRDS coursesRDS,
+  })  : _coursesRDS = coursesRDS,
+        super();
+
+  final CoursesRDS _coursesRDS;
+
+  @override
+  Future<Either<Failure, List<CourseModule>>> fetchCourseModules(
+    String courseId,
+  ) async {
+    try {
+      final courseModuleCollection =
+          _coursesRDS.getCourseModulesReference(courseId);
+      final modulesSnapshot = await courseModuleCollection.get();
+      if (modulesSnapshot.docs.isNotEmpty) {
+        final courseModulesList = modulesSnapshot.docs
+            .map(
+              (snapshot) => snapshot.data(),
+            )
+            .toList();
+        return Right(courseModulesList);
+      } else {
+        return const Right([]);
+      }
+    } catch (e) {
+      return Left(CourseModuleListFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<Course>>> fetchCourses() async {
+    try {
+      final coursesCollection = _coursesRDS.getCoursesReference();
+      final coursesQuery = await coursesCollection.get();
+      final coursesList = coursesQuery.docs.map((e) => e.data()).toList();
+      return Right(coursesList);
+    } catch (e) {
+      return Left(CourseListFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, Course>> getCourseById(String courseId) async {
+    try {
+      final coursesCollection = _coursesRDS.getCoursesReference();
+      final courseDoc = await coursesCollection.doc(courseId).get();
+      if (courseDoc.data() != null) {
+        return Right(courseDoc.data()!);
+      } else {
+        throw Exception();
+      }
+    } catch (e) {
+      return Left(CourseByIdFailure());
+    }
+  }
+}
---
+++ lib/core/error/course_failure.dart
@@ -0,0 +1,21 @@
+import 'package:faeng_courses/core/error/failures.dart';
+
+abstract class CourseFailure extends Failure {}
+
+class CourseByIdFailure extends CourseFailure {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+class CourseModuleListFailure extends CourseFailure {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+class CourseListFailure extends CourseFailure {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
