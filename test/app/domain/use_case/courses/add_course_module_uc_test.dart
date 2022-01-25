6117afd27f44d632e0d6d34591da321ec83cd38d
Alvaro Prado <alvarob.prado@hotmail.com>
2022-01-25 19:08:14 -0400

    Remove unused imports

--- test/app/domain/use_case/courses/add_course_module_uc_test.dart
+++ test/app/domain/use_case/courses/add_course_module_uc_test.dart
@@ -1,9 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:faeng_courses/app/domain/data_repository/courses_data_repository.dart';
-import 'package:faeng_courses/app/domain/entity/course.dart';
 import 'package:faeng_courses/app/domain/entity/course_module.dart';
 import 'package:faeng_courses/app/domain/use_case/courses/add_course_module_uc.dart';
-import 'package:faeng_courses/app/domain/use_case/courses/add_course_uc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
