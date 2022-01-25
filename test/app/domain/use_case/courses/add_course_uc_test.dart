72c085c6f54d04c05f844e1b2bdfa0970f824b90
Alvaro Prado <alvarob.prado@hotmail.com>
2022-01-24 20:30:41 -0400

    Create AddCourseUC and its unit test

---
+++ test/app/domain/use_case/courses/add_course_uc_test.dart
@@ -0,0 +1,55 @@
+import 'package:dartz/dartz.dart';
+import 'package:faeng_courses/app/domain/data_repository/courses_data_repository.dart';
+import 'package:faeng_courses/app/domain/entity/course.dart';
+import 'package:faeng_courses/app/domain/use_case/courses/add_course_uc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'add_course_uc_test.mocks.dart';
+
+@GenerateMocks([Course, CoursesDataRepository])
+void main() {
+  late MockCoursesDataRepository mockRepository;
+  late MockCourse mockCourse;
+  late AddCourseUC useCase;
+  final List<Course> courses = [];
+
+  setUp(() {
+    mockRepository = MockCoursesDataRepository();
+    mockCourse = MockCourse();
+    useCase = AddCourseUC(coursesRepository: mockRepository);
+  });
+
+  test(
+    "should get the list of courses",
+    () async {
+      when(
+        mockRepository.addCourse(any),
+      ).thenAnswer(
+        (invocation) async {
+          courses.add(invocation.positionalArguments.first as Course);
+          return Right(mockCourse);
+        },
+      );
+
+      final eitherResult = await useCase.call(
+        params: AddCourseParam(
+          course: mockCourse,
+        ),
+      );
+
+      eitherResult.fold(
+        (failure) => throw Exception(
+            'UseCase test error, returned ${failure.toString()}'),
+        (success) {
+          expect(success, isA<Course>());
+          expect(courses.length, 1);
+          expect(courses, contains(success));
+        },
+      );
+      verify(mockRepository.addCourse(any));
+
+      verifyNoMoreInteractions(mockRepository);
+    },
+  );
+}
