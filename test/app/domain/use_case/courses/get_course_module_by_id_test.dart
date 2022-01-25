444db4976bf3da4317f53aae9eceeaff2c804dec
Alvaro Prado <alvarob.prado@hotmail.com>
2022-01-25 19:08:14 -0400

    Remove unused imports

--- test/app/domain/use_case/courses/get_course_module_by_id_test.dart
+++ test/app/domain/use_case/courses/get_course_module_by_id_test.dart
@@ -1,9 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:faeng_courses/app/domain/data_repository/courses_data_repository.dart';
-import 'package:faeng_courses/app/domain/entity/course.dart';
 import 'package:faeng_courses/app/domain/entity/course_module.dart';
 import 'package:faeng_courses/app/domain/use_case/courses/get_course_module_by_id_uc.dart';
-import 'package:faeng_courses/core/error/course_failure.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
--- test/app/domain/use_case/courses/get_course_modules_uc_test.dart
+++ test/app/domain/use_case/courses/get_course_modules_uc_test.dart
@@ -1,8 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:faeng_courses/app/domain/data_repository/courses_data_repository.dart';
-import 'package:faeng_courses/app/domain/entity/course.dart';
 import 'package:faeng_courses/app/domain/entity/course_module.dart';
-
 import 'package:faeng_courses/app/domain/use_case/courses/get_course_modules_uc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
