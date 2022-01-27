0b6b5ebe95129abb606a324c8a046f4f26ceed1e
Alvaro Prado <alvarob.prado@hotmail.com>
2022-01-26 20:38:27 -0400

    Create Subject entity and its mappers

--- lib/app/data/mapper/remote_to_domain.dart
+++ lib/app/data/mapper/remote_to_domain.dart
@@ -1,7 +1,9 @@
 import 'package:faeng_courses/app/data/remote/model/course/course_module_rm.dart';
 import 'package:faeng_courses/app/data/remote/model/course/course_rm.dart';
+import 'package:faeng_courses/app/data/remote/model/subject/subject_rm.dart';
 import 'package:faeng_courses/app/domain/entity/course.dart';
 import 'package:faeng_courses/app/domain/entity/course_module.dart';
+import 'package:faeng_courses/app/domain/entity/subject.dart';
 
 extension CourseRMToDM on CourseRM {
   Course toDM() {
@@ -34,5 +36,21 @@ extension CourseModuleRMToDM on CourseModuleRM {
 extension CourseModuleRMListToDMList on List<CourseModuleRM> {
   List<CourseModule> toDMList() {
     return map((courseModule) => courseModule.toDM()).toList();
+  }
+}
+
+extension SubjectRMToDM on SubjectRM {
+  Subject toDM() {
+    return Subject(
+      bannerUrl: bannerUrl,
+      description: description,
+      name: name,
+    );
+  }
+}
+
+extension SubjectRMListToDMList on List<SubjectRM> {
+  List<Subject> toDMList() {
+    return map((subjectRM) => subjectRM.toDM()).toList();
   }
 }
---
+++ lib/app/data/remote/model/subject/subject_rm.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'subject_rm.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SubjectRM _$SubjectRMFromJson(Map<String, dynamic> json) => SubjectRM(
+      bannerUrl: json['bannerUrl'] as String,
+      description: json['description'] as String,
+      name: json['name'] as String,
+    );
+
+Map<String, dynamic> _$SubjectRMToJson(SubjectRM instance) => <String, dynamic>{
+      'bannerUrl': instance.bannerUrl,
+      'description': instance.description,
+      'name': instance.name,
+    };
