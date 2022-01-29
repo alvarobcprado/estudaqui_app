ef2cae3b4917a305d6f2080c09875b69d91da3e6
Alvaro Prado <alvarob.prado@hotmail.com>
2022-01-29 00:05:43 -0400

    Add initial course list page structure

--- lib/common/my_route_map.dart
+++ lib/common/my_route_map.dart
@@ -1,4 +1,5 @@
 import 'package:faeng_courses/app/presentation/pages/base/base_page.dart';
+import 'package:faeng_courses/app/presentation/pages/course_list/course_list_page.dart';
 import 'package:faeng_courses/app/presentation/pages/home/home_page.dart';
 import 'package:faeng_courses/app/presentation/pages/login/login_page.dart';
 import 'package:faeng_courses/app/presentation/pages/not_found/not_found_page.dart';
@@ -7,6 +8,7 @@ import 'package:routemaster/routemaster.dart';
 
 const _basePath = '/';
 const _homePath = '${_basePath}home';
+const _courseListPath = '$_homePath/courses';
 const _loginPath = '${_basePath}login';
 const _notFoundPath = '${_basePath}not-found';
 
@@ -20,11 +22,18 @@ class MyRouteMap extends RouteMap {
                   paths: [
                     _homePath,
                     _notFoundPath,
+                    _courseListPath,
                   ],
                 ),
             _homePath: (_) => const MaterialPage(
                   name: 'Home',
                   child: HomePage(),
+                ),
+            _courseListPath: (routeData) => MaterialPage(
+                  name: 'Course List',
+                  child: CourseListPage(
+                    subject: routeData.queryParameters['subject'] ?? '',
+                  ),
                 ),
             _loginPath: (_) => const MaterialPage(
                   name: 'Login',
@@ -42,6 +51,12 @@ extension MyPageRoutes on Routemaster {
   void pushLogin() => push(_loginPath);
   void pushHome() => push(_homePath);
   void pushTest() => push(_notFoundPath);
+  void pushCoursesOf(String subject) => push(
+        _courseListPath,
+        queryParameters: {
+          'subject': subject,
+        },
+      );
 
   void replaceWithHome() => replace(_homePath);
   void replaceWithNotFound() => replace(_notFoundPath);
