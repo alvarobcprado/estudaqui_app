35e613fe0e4dfa4cfdbf4ea0b773c90b31184f5f
Alvaro Prado <alvarob.prado@hotmail.com>
2022-01-31 20:52:36 -0400

    Add initial courseDetailPage structure

---
+++ lib/app/presentation/pages/course_detail/couse_detail_models.dart
@@ -0,0 +1,32 @@
+import 'package:faeng_courses/app/domain/entity/course_module.dart';
+import 'package:faeng_courses/core/error/failures.dart';
+
+enum CourseDetailStatus { loading, error, success }
+
+class CourseDetailState {
+  CourseDetailState({
+    this.status = CourseDetailStatus.loading,
+    this.moduleList = const <CourseModule>[],
+    this.currentModule,
+    this.failure,
+  });
+  final CourseDetailStatus status;
+  final List<CourseModule> moduleList;
+  final int? currentModule;
+  final Failure? failure;
+
+  CourseDetailState copyWith({
+    String? courseName,
+    CourseDetailStatus? status,
+    List<CourseModule>? moduleList,
+    int? currentModule,
+    Failure? failure,
+  }) {
+    return CourseDetailState(
+      status: status ?? this.status,
+      moduleList: moduleList ?? this.moduleList,
+      currentModule: currentModule ?? this.currentModule,
+      failure: failure ?? this.failure,
+    );
+  }
+}
