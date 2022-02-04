d8e14dd8cb74cca13a94404d20d37b2f964bef97
Alvaro Prado <alvarob.prado@hotmail.com>
2022-02-03 21:49:15 -0400

    Add initial home page structure

--- lib/app/presentation/pages/home/home_page.dart
+++ lib/app/presentation/pages/home/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:faeng_courses/app/presentation/common/utils/constants.dart';
+import 'package:faeng_courses/app/presentation/pages/home/home_models.dart';
+import 'package:faeng_courses/app/presentation/pages/home/home_page_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:routemaster/routemaster.dart';
@@ -11,24 +14,78 @@ class HomePage extends StatelessWidget {
     return Consumer(
       builder: (context, ref, child) {
         return Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Center(
-              child: Text('home page'),
+            SizedBox(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 3,
+              child: Card(
+                color: Colors.grey.shade200,
+                elevation: 3.0,
+                child: const Center(
+                  child: Text('Texto inicial temporário'),
+                ),
+              ),
             ),
-            TextButton(
-              onPressed: () => Routemaster.of(context).pushCoursesOf('math'),
-              child: const Text('Go to Math courses'),
-            ),
-            TextButton(
-              onPressed: () =>
-                  Routemaster.of(context).pushCoursesOf('portuguese'),
-              child: const Text('Go to Portuguese courses'),
-            ),
-            TextButton(
-              onPressed: () => Routemaster.of(context).pushAllCourses(),
-              child: const Text('Go to All courses'),
+            const SizedBox(height: 15),
+            Consumer(
+              builder: (context, ref, child) {
+                final state = ref.watch(homePageNotifierProvider);
+                switch (state.status) {
+                  case HomeStatus.loading:
+                    return const CircularProgressIndicator();
+                  case HomeStatus.error:
+                    return Center(
+                      child: Text(
+                        state.failure.toString(),
+                      ),
+                    );
+                  case HomeStatus.success:
+                    return SizedBox(
+                      width: double.infinity,
+                      child: Wrap(
+                        alignment: WrapAlignment.spaceEvenly,
+                        children: state.subjectList
+                            .where((subject) => subject.isActive)
+                            .map(
+                              (subject) => GestureDetector(
+                                onTap: () =>
+                                    Routemaster.of(context).pushCoursesOf(
+                                  subject.subjectId,
+                                ),
+                                child: SizedBox(
+                                  width: 125,
+                                  height: 100,
+                                  child: Card(
+                                    color: Colors.grey.shade200,
+                                    elevation: 3.0,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          subject.name
+                                              .substring(0, 3)
+                                              .toUpperCase(),
+                                        ),
+                                        Text(
+                                          subject.description,
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    );
+                }
+              },
             ),
           ],
         );
--- lib/app/presentation/pages/home/home_page_notifier.dart
+++ lib/app/presentation/pages/home/home_page_notifier.dart
@@ -1,1 +1,39 @@
+import 'package:faeng_courses/app/domain/use_case/use_case.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:faeng_courses/app/domain/use_case/subject/get_subjects_uc.dart';
+import 'package:faeng_courses/app/presentation/pages/home/home_models.dart';
+import 'package:faeng_courses/common/providers/use_case/subject_usecase_provider.dart';
+
+class HomePageNotifier extends StateNotifier<HomeState> {
+  HomePageNotifier({
+    required this.getSubjectsUC,
+  }) : super(const HomeState(status: HomeStatus.loading)) {
+    getSubjects();
+  }
+
+  final GetSubjectsUC getSubjectsUC;
+
+  Future<void> getSubjects() async {
+    state = state.copyWith(status: HomeStatus.loading);
+
+    final eitherResult = await getSubjectsUC.call(params: NoParams());
+
+    state = eitherResult.fold(
+      (failure) {
+        return state.copyWith(status: HomeStatus.error, failure: failure);
+      },
+      (success) {
+        return state.copyWith(status: HomeStatus.success, subjectList: success);
+      },
+    );
+  }
+}
+
+final homePageNotifierProvider =
+    StateNotifierProvider<HomePageNotifier, HomeState>(
+  (ref) {
+    final getSubjectsUC = ref.watch(getSubjectsUCProvider);
+    return HomePageNotifier(getSubjectsUC: getSubjectsUC);
+  },
+);
