6713043e8b47a81b5bea555a267c8e99b17110cf
Alvaro Prado <alvarob.prado@hotmail.com>
2022-02-04 23:44:24 -0400

    Extract subjectList Widget

--- lib/app/presentation/pages/home/home_page.dart
+++ lib/app/presentation/pages/home/home_page.dart
@@ -1,11 +1,8 @@
 import 'package:faeng_courses/app/presentation/common/utils/constants.dart';
-import 'package:faeng_courses/app/presentation/pages/home/home_models.dart';
-import 'package:faeng_courses/app/presentation/pages/home/home_page_notifier.dart';
+import 'package:faeng_courses/app/presentation/pages/home/subject_list/subject_list.dart';
 import 'package:faeng_courses/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:routemaster/routemaster.dart';
-import 'package:faeng_courses/common/my_route_map.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -49,67 +46,7 @@ class HomePage extends StatelessWidget {
               ),
             ),
             const SizedBox(height: kSmallSpacer),
-            Consumer(
-              builder: (context, ref, child) {
-                final state = ref.watch(homePageNotifierProvider);
-                switch (state.status) {
-                  case HomeStatus.loading:
-                    return const CircularProgressIndicator();
-                  case HomeStatus.error:
-                    return Center(
-                      child: Text(
-                        state.failure.toString(),
-                      ),
-                    );
-                  case HomeStatus.success:
-                    return SizedBox(
-                      width: double.infinity,
-                      child: Wrap(
-                        alignment: WrapAlignment.spaceBetween,
-                        children: state.subjectList
-                            .where((subject) => subject.isActive)
-                            .map(
-                              (subject) => GestureDetector(
-                                onTap: () =>
-                                    Routemaster.of(context).pushCoursesOf(
-                                  subject.subjectId,
-                                ),
-                                child: Material(
-                                  elevation: 5,
-                                  color: Colors.grey.shade200,
-                                  borderRadius: BorderRadius.circular(
-                                    kMediumBorder,
-                                  ),
-                                  child: SizedBox(
-                                    width: 125,
-                                    height: 110,
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          subject.name
-                                              .substring(0, 3)
-                                              .toUpperCase(),
-                                        ),
-                                        Text(
-                                          subject.description,
-                                          textAlign: TextAlign.center,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            )
-                            .toList(),
-                      ),
-                    );
-                }
-              },
-            ),
+            const SubjectList(),
           ],
         );
       },
