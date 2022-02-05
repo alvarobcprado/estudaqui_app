5606da19461332a3d5a8483863a7426ade6159a1
Alvaro Prado <alvarob.prado@hotmail.com>
2022-02-04 22:25:43 -0400

    Adjust HomePage design and add intl text

--- lib/app/presentation/pages/course_detail/course_detail_page_notifier.dart
+++ lib/app/presentation/pages/course_detail/course_detail_page_notifier.dart
@@ -1,4 +1,3 @@
-import 'package:faeng_courses/app/domain/use_case/courses/get_course_by_id_uc.dart';
 import 'package:faeng_courses/app/domain/use_case/courses/get_course_modules_uc.dart';
 import 'package:faeng_courses/common/general_providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
