ff1bd9d082ba6d31d2adc10ebed5d732785a8a93
Alvaro Prado <alvarob.prado@hotmail.com>
2022-03-22 19:34:02 -0400

    fix(ActionHandler): change to ConsumerWidget

--- lib/app/presentation/common/widgets/action_handler.dart
+++ lib/app/presentation/common/widgets/action_handler.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ActionHandler<T> extends StatelessWidget {
+class ActionHandler<T> extends ConsumerWidget {
   const ActionHandler({
     Key? key,
     required this.child,
     required this.actionProvider,
-    required this.ref,
     required this.onReceived,
   }) : super(key: key);
 
   final Widget child;
   final ProviderListenable<T> actionProvider;
-  final WidgetRef ref;
   final void Function(T? previousState, T newState) onReceived;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     ref.listen<T>(
       actionProvider,
       onReceived,
