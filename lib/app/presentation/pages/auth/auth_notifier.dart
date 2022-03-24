6805cf96b76596534f4f0883ea6ad767a0a2ee92
Alvaro Prado <alvarob.prado@hotmail.com>
2022-03-24 18:42:37 -0400

    fix(AuthNotifier): adjust signup flow

--- lib/app/presentation/pages/auth/auth_notifier.dart
+++ lib/app/presentation/pages/auth/auth_notifier.dart
@@ -1,41 +1,46 @@
 import 'package:faeng_courses/app/domain/use_case/authentication/get_current_user_uc.dart';
 import 'package:faeng_courses/app/domain/use_case/authentication/signin_anonmously_uc.dart';
 import 'package:faeng_courses/app/domain/use_case/authentication/signin_email_password_uc.dart';
+import 'package:faeng_courses/app/domain/use_case/authentication/signup_email_password_uc.dart';
 import 'package:faeng_courses/app/domain/use_case/use_case.dart';
 import 'package:faeng_courses/app/presentation/pages/auth/state/auth_state.dart';
 import 'package:faeng_courses/common/general_providers.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final authNotifierProvider =
-    StateNotifierProvider<AuthContainerNotifier, AuthState>(
+    StateNotifierProvider<LoginContainerNotifier, AuthState>(
   (ref) {
     final signinAnonmouslyUC = ref.watch(signinAnonmouslyUCProvider);
     final signinEmailPasswordUC = ref.watch(signinEmailPasswordUCProvider);
     final getCurrentUserUC = ref.watch(getCurrentUserUCProvider);
-    return AuthContainerNotifier(
+    final signupEmailPasswordUC = ref.watch(signupEmailPasswordUCProvider);
+
+    return LoginContainerNotifier(
       signinAnonmouslyUC: signinAnonmouslyUC,
       signinEmailPasswordUC: signinEmailPasswordUC,
+      signupEmailPasswordUC: signupEmailPasswordUC,
       getCurrentUserUC: getCurrentUserUC,
     );
   },
 );
 
-class AuthContainerNotifier extends StateNotifier<AuthState> {
-  AuthContainerNotifier({
+class LoginContainerNotifier extends StateNotifier<AuthState> {
+  LoginContainerNotifier({
     required SigninEmailPasswordUC signinEmailPasswordUC,
     required SigninAnonmouslyUC signinAnonmouslyUC,
     required GetCurrentUserUC getCurrentUserUC,
+    required SignupEmailPasswordUC signupEmailPasswordUC,
   })  : _signinEmailPasswordUC = signinEmailPasswordUC,
         _signinAnonmouslyUC = signinAnonmouslyUC,
         _getCurrentUserUC = getCurrentUserUC,
+        _signupEmailPasswordUC = signupEmailPasswordUC,
         super(AuthState.unauthenticated()) {
     verifyAuthState();
   }
 
   final SigninEmailPasswordUC _signinEmailPasswordUC;
   final SigninAnonmouslyUC _signinAnonmouslyUC;
+  final SignupEmailPasswordUC _signupEmailPasswordUC;
   final GetCurrentUserUC _getCurrentUserUC;
 
   Future<void> verifyAuthState() async {
@@ -54,6 +59,32 @@ class AuthContainerNotifier extends StateNotifier<AuthState> {
           await loginAnonmously();
           return AuthState.authenticated(method: AuthMethod.anonmously);
         }
+      },
+    );
+  }
+
+  Future<void> registerUser({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    state = AuthState.authenticating();
+    final registerAttemptEither = await _signupEmailPasswordUC(
+      params: SignupEmailPasswordParams(
+        name: name,
+        email: email,
+        password: password,
+      ),
+    );
+
+    state = await registerAttemptEither.fold(
+      (failure) => AuthState.failedAuthentication(
+        errorTitle: failure.title,
+        errorBody: failure.message,
+      ),
+      (success) async {
+        await success.updateDisplayName(name);
+        return AuthState.authenticated(method: AuthMethod.emailPassword);
       },
     );
   }
@@ -92,20 +123,5 @@ class AuthContainerNotifier extends StateNotifier<AuthState> {
       ),
       (success) => AuthState.authenticated(method: AuthMethod.anonmously),
     );
-  }
-
-  void validateCurrentFormAndLoginEmailPassword(
-    GlobalKey<FormBuilderState> formKey,
-  ) {
-    final formSaveResult = formKey.currentState?.saveAndValidate();
-
-    if (formSaveResult != null && formSaveResult) {
-      final email = formKey.currentState!.value['emailField'];
-      final password = formKey.currentState!.value['passwordField'];
-      loginEmailPassword(
-        email: email,
-        password: password,
-      );
-    }
   }
 }
