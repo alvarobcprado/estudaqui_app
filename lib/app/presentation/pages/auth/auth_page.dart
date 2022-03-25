ec6b50969b93cb5c50c1c17917e04ae34e8ad0bd
Alvaro Prado <alvarob.prado@hotmail.com>
2022-03-24 21:37:19 -0400

    feat(Auth): create page to handler auth modes

--- lib/app/presentation/pages/auth/auth_page.dart
+++ lib/app/presentation/pages/auth/auth_page.dart
@@ -1,13 +1,19 @@
 import 'package:faeng_courses/app/presentation/common/utils/constants.dart';
 import 'package:faeng_courses/app/presentation/common/widgets/action_handler.dart';
-import 'package:faeng_courses/app/presentation/pages/auth/auth_container.dart/auth_container.dart';
+import 'package:faeng_courses/app/presentation/common/widgets/custom_gradient.dart';
+import 'package:faeng_courses/app/presentation/common/widgets/dialogs.dart';
 import 'package:faeng_courses/app/presentation/pages/auth/auth_notifier.dart';
+import 'package:faeng_courses/app/presentation/pages/auth/login_container.dart/login_container.dart';
+import 'package:faeng_courses/app/presentation/pages/auth/signup_container.dart/signup_container.dart';
 import 'package:faeng_courses/app/presentation/pages/auth/state/auth_state.dart';
+import 'package:faeng_courses/app/presentation/pages/auth/widgets/form_select_widget.dart';
 import 'package:faeng_courses/common/general_providers.dart';
 import 'package:faeng_courses/common/my_route_map.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+
+enum AuthMode { signIn, signUp }
 
 class AuthPage extends StatefulWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -17,14 +23,41 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
+  AuthMode authMode = AuthMode.signIn;
+
+  void onAuthenticated(AuthMethod method) {
+    if (method == AuthMethod.emailPassword) {
+      GoRouter.of(context).replaceWithHome();
+    }
+  }
+
+  void onFailAuthenticated(String? title, String? reason) async {
+    await DialogHandler.showAlertDialog(
+      context,
+      title: title,
+      body: reason,
+    );
+  }
+
+  void onAuthenticating() async {
+    await DialogHandler.showLoadingDialog(context);
+  }
+
+  void switchForm(AuthMode mode) {
+    setState(() {
+      authMode = mode;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer(
       builder: (context, ref, child) {
         final colors = ref.watch(themeProvider).colors;
+        final assets = ref.watch(themeProvider).assets;
         final authNotifier = ref.watch(authNotifierProvider.notifier);
         return Scaffold(
-          backgroundColor: colors.loginPageBackground,
+          resizeToAvoidBottomInset: true,
           body: ActionHandler<AuthState>(
             actionProvider: authNotifierProvider,
             onReceived: (oldState, newState) {
@@ -32,56 +65,43 @@ class _AuthPageState extends State<AuthPage> {
                 authenticating: () => Navigator.of(context).pop(),
               );
               newState.maybeWhen(
-                authenticated: (method) {
-                  if (method == AuthMethod.emailPassword) {
-                    GoRouter.of(context).replaceWithHome();
-                  }
-                },
-                failedAuthentication: (title, reason) async {
-                  await showDialog(
-                    context: context,
-                    builder: (ctx) => AlertDialog(
-                      title: Text(title ?? 'Problema ao logar'),
-                      content: Text(
-                        reason ??
-                            'Algo deu errado ao fazer o login.\nTente novamente daqui a pouco!',
-                      ),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('Fechar'),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-                authenticating: () async {
-                  await showDialog(
-                    context: context,
-                    builder: (ctx) => Dialog(
-                      child: Padding(
-                        padding: const EdgeInsets.all(kMediumPadding),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: const [
-                            CircularProgressIndicator(),
-                            SizedBox(
-                              height: kSmallSpacer,
-                            ),
-                            Text('Autenticando...'),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                },
+                authenticated: onAuthenticated,
+                failedAuthentication: onFailAuthenticated,
+                authenticating: onAuthenticating,
                 orElse: () {},
               );
             },
-            child: AuthContainer(
-              onValidated: authNotifier.loginEmailPassword,
+            child: SingleChildScrollView(
+              child: CustomGradient(
+                child: SafeArea(
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: kLargeNumber),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: kXXXLargeNumber),
+                        Image.asset(assets.logoOrange),
+                        const SizedBox(height: kXXLargeNumber),
+                        FormSelectWidget(
+                          colors: colors,
+                          onSwitch: switchForm,
+                        ),
+                        const SizedBox(height: kSmallNumber),
+                        AnimatedSwitcher(
+                          duration: const Duration(milliseconds: 250),
+                          child: authMode == AuthMode.signIn
+                              ? LoginContainer(
+                                  onValidated: authNotifier.loginEmailPassword,
+                                )
+                              : SignUpContainer(
+                                  onValidated: authNotifier.registerUser,
+                                ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
         );
