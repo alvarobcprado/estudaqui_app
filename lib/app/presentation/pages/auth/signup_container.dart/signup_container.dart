4f8a5b98ddc4b84b4f706024791f0765c579c1e0
Alvaro Prado <alvarob.prado@hotmail.com>
2022-03-24 21:36:24 -0400

    feat(SignupForm): create form to signup

---
+++ lib/app/presentation/pages/auth/signup_container.dart/signup_container.dart
@@ -0,0 +1,80 @@
+import 'package:faeng_courses/app/presentation/common/utils/constants.dart';
+import 'package:faeng_courses/app/presentation/common/widgets/my_button.dart';
+import 'package:faeng_courses/app/presentation/pages/auth/widgets/signup_form_widget.dart';
+import 'package:faeng_courses/common/general_providers.dart';
+import 'package:faeng_courses/generated/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+typedef RegisterCallback = Future<void> Function({
+  required String name,
+  required String email,
+  required String password,
+});
+
+class SignUpContainer extends StatefulWidget {
+  const SignUpContainer({
+    Key? key,
+    required this.onValidated,
+  }) : super(key: key);
+
+  final RegisterCallback onValidated;
+
+  @override
+  State<SignUpContainer> createState() => _SignUpContainerState();
+}
+
+class _SignUpContainerState extends State<SignUpContainer> {
+  final _formKey = GlobalKey<FormBuilderState>();
+  late String _name = '';
+  late String _email = '';
+  late String _password = '';
+
+  bool _isValidForm() {
+    final formSaveResult = _formKey.currentState?.saveAndValidate();
+
+    if (formSaveResult != null && formSaveResult) {
+      _name = _formKey.currentState!.value['nameField'];
+      _email = _formKey.currentState!.value['emailField'];
+      _password = _formKey.currentState!.value['passwordField'];
+      return true;
+    }
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          SignUpFormWidget(formKey: _formKey),
+          const SizedBox(height: kXXLargeNumber),
+          Consumer(builder: (context, ref, child) {
+            final colors = ref.watch(themeProvider).colors;
+            return Row(
+              children: [
+                Expanded(
+                  child: MyButton(
+                    onPressed: () async {
+                      if (_isValidForm()) {
+                        widget.onValidated(
+                          name: _name,
+                          email: _email,
+                          password: _password,
+                        );
+                      }
+                    },
+                    textColor: colors.primaryColor,
+                    backgrounColor: colors.white,
+                    text: S.of(context).auth_form_signup_button,
+                  ),
+                ),
+              ],
+            );
+          }),
+        ],
+      ),
+    );
+  }
+}
