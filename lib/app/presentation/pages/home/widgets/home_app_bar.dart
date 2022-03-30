e0e8b1285d208d55a48848bad95e63c6b9c0d110
Alvaro Prado <alvarob.prado@hotmail.com>
2022-03-30 18:42:29 -0400

    feat(HomeAppBar): create custom home app bar

---
+++ lib/app/presentation/pages/home/widgets/home_app_bar.dart
@@ -0,0 +1,103 @@
+import 'package:faeng_courses/app/presentation/common/utils/constants.dart';
+import 'package:faeng_courses/common/general_providers.dart';
+import 'package:faeng_courses/generated/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeAppBar extends SliverPersistentHeaderDelegate {
+  const HomeAppBar({
+    this.expandedHeight = 188 + 24,
+    required this.userName,
+  });
+
+  final double? expandedHeight;
+  final String userName;
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    double progress = shrinkOffset / expandedHeight!;
+    return Consumer(
+      builder: (context, ref, _) {
+        final colors = ref.watch(themeProvider).colors;
+        final textStyles = ref.watch(themeProvider).textStyles;
+        return Container(
+          decoration: BoxDecoration(
+            color: colors.primaryColor,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(
+                96 * (1 - progress),
+              ),
+            ),
+          ),
+          child: Stack(
+            clipBehavior: Clip.hardEdge,
+            fit: StackFit.expand,
+            children: [
+              Opacity(
+                opacity: (1 - progress),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: kXXLargeNumber,
+                    top: kXXXXLargeNumber,
+                  ),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text.rich(
+                          TextSpan(
+                            text: S.of(context).home_greeting_user,
+                            style: textStyles.homeAppBarGreeting,
+                            children: [
+                              TextSpan(
+                                text: userName,
+                                style: textStyles.homeAppBarUserName,
+                              ),
+                              const TextSpan(text: ','),
+                            ],
+                          ),
+                        ),
+                        Text(
+                          S.of(context).home_welcome_message,
+                          style: textStyles.homeAppBarWelcome,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Opacity(
+                  opacity: progress,
+                  child: Text(
+                    S.of(context).home_page_title,
+                    style: textStyles.homeAppBarTitle,
+                  ),
+                ),
+              ),
+              AppBar(
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  double get maxExtent => expandedHeight!;
+
+  @override
+  double get minExtent => kToolbarHeight;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      true;
+}
