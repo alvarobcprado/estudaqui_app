bb97de5737093792af7e2d3df038fab7ac96941d
Alvaro Prado <alvarob.prado@hotmail.com>
2022-03-30 19:07:33 -0400

    feat(SubjectsSliverGrid): create sliverGrid to show the subjects

---
+++ lib/app/presentation/pages/home/widgets/subjects_sliver_grid.dart
@@ -0,0 +1,37 @@
+import 'package:faeng_courses/app/domain/entity/subject.dart';
+import 'package:faeng_courses/app/presentation/pages/home/widgets/subjects_grid_item.dart';
+import 'package:faeng_courses/common/my_route_map.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SubjectsSliverGrid extends StatelessWidget {
+  const SubjectsSliverGrid({
+    Key? key,
+    required this.subjectList,
+  }) : super(key: key);
+
+  final List<Subject> subjectList;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 24,
+        mainAxisSpacing: 24,
+      ),
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          final subject = subjectList[index];
+          return SubjectsGridItem(
+            name: subject.name,
+            imageUrl: subject.bannerUrl,
+            onPressed: () =>
+                GoRouter.of(context).pushCoursesOf(subject.subjectId),
+          );
+        },
+        childCount: subjectList.length,
+      ),
+    );
+  }
+}
