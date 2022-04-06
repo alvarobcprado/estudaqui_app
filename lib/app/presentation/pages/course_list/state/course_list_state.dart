1c399b5d67f2cd9f839bc9d99b6b743fed3fa5a2
Alvaro Prado <alvarob.prado@hotmail.com>
2022-04-05 23:14:58 -0400

    feat(CourseListState): create states of course listing

---
+++ lib/app/presentation/pages/course_list/state/course_list_state.dart
@@ -0,0 +1,18 @@
+import 'package:faeng_courses/app/domain/entity/course.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'course_list_state.freezed.dart';
+
+@freezed
+class CourseListState with _$CourseListState {
+  factory CourseListState.initial() = Initial;
+  factory CourseListState.initialLoading() = InitialLoading;
+  factory CourseListState.success({
+    required List<Course> courseList,
+  }) = Success;
+  factory CourseListState.empty() = Empty;
+  factory CourseListState.failed({
+    String? errorTitle,
+    String? errorBody,
+  }) = Failed;
+}
