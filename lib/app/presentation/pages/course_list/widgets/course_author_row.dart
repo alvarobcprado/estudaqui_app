d6ef7828eab64e4d2a65ce088a15e046bb629e34
Alvaro Prado <alvarob.prado@hotmail.com>
2022-04-06 20:52:00 -0400

    feat(CourseAuthorRow): create course author identification row

---
+++ lib/app/presentation/pages/course_list/widgets/course_author_row.dart
@@ -0,0 +1,42 @@
+import 'package:faeng_courses/app/presentation/common/utils/constants.dart';
+import 'package:faeng_courses/common/general_providers.dart';
+import 'package:faeng_courses/generated/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CourseAuthorRow extends ConsumerWidget {
+  const CourseAuthorRow({
+    Key? key,
+    required this.authorName,
+  }) : super(key: key);
+
+  final String authorName;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(themeProvider);
+    final colors = theme.colors;
+    final textStyles = theme.textStyles;
+
+    final name =
+        authorName.isEmpty ? S.of(context).default_course_author : authorName;
+    return Row(
+      children: [
+        CircleAvatar(
+          minRadius: kSmallNumber,
+          maxRadius: kSmallNumber * 1.5,
+          backgroundColor: colors.secondaryVariantColor,
+          child: Text(
+            name.characters.first,
+            style: textStyles.courseListAuthorRow,
+          ),
+        ),
+        const SizedBox(width: kSmallNumber / 2),
+        Text(
+          name,
+          style: textStyles.courseListAuthorRow,
+        ),
+      ],
+    );
+  }
+}
