7557f5e798e78581af720fb8491787bbb6cfc6dd
Alvaro Prado <alvarob.prado@hotmail.com>
2022-04-08 21:13:12 -0400

    refact(HomeSubjectsSection): adjust states

--- lib/app/presentation/pages/home/widgets/home_subjects_section.dart
+++ lib/app/presentation/pages/home/widgets/home_subjects_section.dart
@@ -1,7 +1,8 @@
 import 'package:faeng_courses/app/presentation/common/utils/constants.dart';
-import 'package:faeng_courses/app/presentation/common/widgets/error_handler_widget.dart';
+import 'package:faeng_courses/app/presentation/common/widgets/unexpected_state_widget.dart';
 import 'package:faeng_courses/app/presentation/pages/home/home_page_notifier.dart';
 import 'package:faeng_courses/app/presentation/pages/home/widgets/subjects_sliver_grid.dart';
+import 'package:faeng_courses/common/general_providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -13,17 +14,22 @@ class HomeSubjectsSection extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final subjects = ref.watch(getSubjectsProvider);
+    final colors = ref.watch(themeProvider).colors;
     return SliverPadding(
       padding: const EdgeInsets.symmetric(horizontal: kLargeNumber),
       sliver: subjects.maybeWhen(
         orElse: () => SliverToBoxAdapter(
-          child: ErrorHandlerWidget(
-            child: const SizedBox(),
+          child: UnexpectedStateWidget(
             onTryAgain: () => ref.refresh(getSubjectsProvider),
           ),
         ),
-        loading: () => const SliverToBoxAdapter(
-          child: Center(child: CircularProgressIndicator()),
+        loading: () => SliverFillRemaining(
+          hasScrollBody: false,
+          child: Center(
+            child: CircularProgressIndicator(
+              color: colors.secondaryVariantColor,
+            ),
+          ),
         ),
         data: (subjectList) => SubjectsSliverGrid(
           subjectList: subjectList,
