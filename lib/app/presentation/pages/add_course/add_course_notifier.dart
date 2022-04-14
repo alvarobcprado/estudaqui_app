5230b1fcea6c76e3a5f4c2ec7413d9a9adc35587
Alvaro Prado <alvarob.prado@hotmail.com>
2022-04-13 21:31:46 -0400

    feat(AddCourseNotifier): update add course notifiers

--- lib/app/presentation/pages/add_course/add_course_notifier.dart
+++ lib/app/presentation/pages/add_course/add_course_notifier.dart
@@ -1,9 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:faeng_courses/app/domain/entity/course.dart';
 import 'package:faeng_courses/app/domain/entity/course_module.dart';
+import 'package:faeng_courses/app/domain/entity/subject.dart';
 import 'package:faeng_courses/app/domain/use_case/courses/add_course_and_module_uc.dart';
-import 'package:faeng_courses/app/presentation/pages/add_course/add_course_models.dart';
+import 'package:faeng_courses/app/domain/use_case/use_case.dart';
+import 'package:faeng_courses/app/presentation/pages/add_course/models/add_course_form_model.dart';
+import 'package:faeng_courses/app/presentation/pages/add_course/state/add_course_state.dart';
 import 'package:faeng_courses/core/common/general_providers.dart';
+import 'package:faeng_courses/core/common/providers/use_case/subject_usecase_provider.dart';
 import 'package:faeng_courses/core/error/failure.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
@@ -20,49 +24,70 @@ final addCourseNotifierProvider =
 );
 
 final formBuilderKeyProvider =
-    Provider.autoDispose<GlobalKey<FormBuilderState>>((ref) {
-  return GlobalKey<FormBuilderState>();
-});
+    Provider.autoDispose<GlobalKey<FormBuilderState>>(
+  (ref) {
+    return GlobalKey<FormBuilderState>();
+  },
+);
+
+final _getSubjectsProvider = FutureProvider<List<Subject>>(
+  (ref) async {
+    final getSubjectsUC = ref.watch(getSubjectsUCProvider);
+    final eitherResult = await getSubjectsUC.call(params: NoParams());
+    return eitherResult.fold((failure) => [], (success) => success);
+  },
+);
+
+final courseSubjectsProvider = StateProvider.autoDispose<List<Subject>>(
+  (ref) {
+    final asyncSubjects = ref.watch(_getSubjectsProvider);
+    return asyncSubjects.when(
+      data: (subjects) => subjects,
+      error: (_, __) => [],
+      loading: () => [],
+    );
+  },
+);
+
+final courseContentProvider = StateProvider.autoDispose<String?>(
+  (ref) {
+    return null;
+  },
+);
 
 class AddCourseNotifier extends StateNotifier<AddCourseState> {
   AddCourseNotifier({
     required AddCourseAndModuleUC addCourseAndModuleUC,
   })  : _addCourseAndModuleUC = addCourseAndModuleUC,
-        super(
-          const AddCourseState(
-            status: AddCourseStatus.loading,
-          ),
-        );
+        super(const AddCourseState.initial());
 
   final AddCourseAndModuleUC _addCourseAndModuleUC;
+
+  bool _validateCurrentForm(GlobalKey<FormBuilderState> formKey) {
+    final formSaveResult = formKey.currentState?.saveAndValidate();
+    return formSaveResult != null && formSaveResult;
+  }
 
   Future<void> validateCurrentFormAndAddCourse(
     GlobalKey<FormBuilderState> formKey,
   ) async {
-    state = state.copyWith(status: AddCourseStatus.loading);
-    final formSaveResult = formKey.currentState?.saveAndValidate();
+    state = const AddCourseState.loading();
     late Either<Failure, CourseModule> eitherResult;
 
-    if (formSaveResult != null && formSaveResult) {
+    if (_validateCurrentForm(formKey)) {
       final formValue = formKey.currentState!.value;
-      final courseName = formValue['courseNameField'];
-      final courseDescription = formValue['courseDescriptionField'];
-      final courseImage = formValue['courseImageField'];
-      final courseSubject = formValue['courseSubjectField'];
-      const courseModuleName = 'Módulo 1';
-      final courseModuleText = formValue['courseModuleTextField'];
-      // final courseModuleName = formValue['courseModuleNameField'];
-      // final courseModuleText = formValue['courseModuleTextField'];
+
+      final courseFormInfos = AddCourseFormModel.fromJson(formValue);
 
       final courseToAdd = Course(
         courseId: '',
         creatorId: '',
-        subject: courseSubject,
-        title: courseName,
-        subtitle: courseDescription,
+        subject: courseFormInfos.courseSubject,
+        title: courseFormInfos.courseName,
+        subtitle: courseFormInfos.courseDescription,
         createdAt: DateTime.now(),
         projectId: '',
-        bannerUrl: courseImage,
+        bannerUrl: courseFormInfos.courseImage,
         updatedAt: DateTime.now(),
       );
 
@@ -70,8 +95,8 @@ class AddCourseNotifier extends StateNotifier<AddCourseState> {
         index: 0,
         moduleId: '',
         courseId: '',
-        name: courseModuleName,
-        text: courseModuleText,
+        name: 'Módulo 1',
+        text: courseFormInfos.courseContent,
       );
 
       eitherResult = await _addCourseAndModuleUC(
@@ -82,9 +107,13 @@ class AddCourseNotifier extends StateNotifier<AddCourseState> {
       );
 
       state = eitherResult.fold(
-        (l) => state.copyWith(status: AddCourseStatus.error),
-        (r) => state.copyWith(status: AddCourseStatus.success),
+        (l) => AddCourseState.error(errorTitle: l.title, errorBody: l.message),
+        (r) => const AddCourseState.success(),
       );
     }
+    state = const AddCourseState.error(
+      errorTitle: 'Problema ao adicionar curso',
+      errorBody: 'Por favor, verifique os campos do formulário x.x',
+    );
   }
 }
