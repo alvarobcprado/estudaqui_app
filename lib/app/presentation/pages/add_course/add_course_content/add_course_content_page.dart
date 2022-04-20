1b81079f4b130cc485108b9fa152bd836bbbf520
Alvaro Prado <alvarob.prado@hotmail.com>
2022-04-20 17:19:57 -0400

    feat(AddCourseContent): add the editor to create course content

--- lib/app/presentation/pages/add_course/add_course_content/add_course_content_page.dart
+++ lib/app/presentation/pages/add_course/add_course_content/add_course_content_page.dart
@@ -1,29 +1,18 @@
-import 'package:faeng_courses/app/presentation/common/utils/constants.dart';
-import 'package:faeng_courses/app/presentation/pages/add_course/add_course_notifier.dart';
+import 'package:faeng_courses/app/presentation/pages/add_course/add_course_content/widgets/sliver_html_editor.dart';
 import 'package:faeng_courses/generated/l10n.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AddCourseContentDialog extends ConsumerWidget {
+class AddCourseContentDialog extends StatelessWidget {
   const AddCourseContentDialog({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
             leading: IconButton(
-              onPressed: () {
-                ref
-                    .read(formBuilderKeyProvider)
-                    .currentState
-                    ?.fields['courseModuleTextField']
-                    ?.didChange(
-                      ref.read(courseContentProvider),
-                    );
-                Navigator.of(context).pop();
-              },
+              onPressed: () => Navigator.of(context).pop(),
               icon: const Icon(Icons.close),
             ),
             floating: true,
@@ -31,41 +20,11 @@ class AddCourseContentDialog extends ConsumerWidget {
             actions: [
               IconButton(
                 icon: const Icon(Icons.save),
-                onPressed: () {
-                  ref
-                      .read(formBuilderKeyProvider)
-                      .currentState
-                      ?.fields['courseModuleTextField']
-                      ?.didChange(
-                        ref.read(courseContentProvider),
-                      );
-
-                  Navigator.of(context).pop();
-                },
+                onPressed: () => Navigator.of(context).pop(),
               ),
             ],
           ),
-          SliverFillRemaining(
-            hasScrollBody: false,
-            child: Padding(
-              padding: const EdgeInsets.all(kSmallPadding),
-              child: Column(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      initialValue: ref.watch(courseContentProvider),
-                      textAlignVertical: TextAlignVertical.top,
-                      onChanged: (value) => ref
-                          .read(courseContentProvider.notifier)
-                          .update((state) => state = value),
-                      maxLines: null,
-                      expands: true,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          SliverHtmlEditor(),
         ],
       ),
     );
