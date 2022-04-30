2a72ab18aabebcda088588ebb7549cd49fd0c499
Alvaro Prado <alvarob.prado@hotmail.com>
2022-04-29 21:23:18 -0400

    refact(HomePage): adjust loading indicator color

--- lib/app/presentation/pages/home/widgets/home_subjects_section.dart
+++ lib/app/presentation/pages/home/widgets/home_subjects_section.dart
@@ -2,7 +2,6 @@ import 'package:faeng_courses/app/presentation/common/utils/constants.dart';
 import 'package:faeng_courses/app/presentation/common/widgets/unexpected_state_widget.dart';
 import 'package:faeng_courses/app/presentation/pages/home/home_page_notifier.dart';
 import 'package:faeng_courses/app/presentation/pages/home/widgets/subjects_sliver_grid.dart';
-import 'package:faeng_courses/core/common/general_providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,7 +13,6 @@ class HomeSubjectsSection extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final subjects = ref.watch(getSubjectsProvider);
-    final colors = ref.watch(themeProvider).colors;
     return SliverPadding(
       padding: const EdgeInsets.symmetric(horizontal: kLargeNumber),
       sliver: subjects.maybeWhen(
@@ -23,12 +21,10 @@ class HomeSubjectsSection extends ConsumerWidget {
             onTryAgain: () => ref.refresh(getSubjectsProvider),
           ),
         ),
-        loading: () => SliverFillRemaining(
+        loading: () => const SliverFillRemaining(
           hasScrollBody: false,
           child: Center(
-            child: CircularProgressIndicator(
-              color: colors.secondaryVariantColor,
-            ),
+            child: CircularProgressIndicator(),
           ),
         ),
         data: (subjectList) => SubjectsSliverGrid(
