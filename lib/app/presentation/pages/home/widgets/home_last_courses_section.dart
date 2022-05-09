695072c02447fc5027dcd8a5a75762c1b7bdc07c
Alvaro Prado <alvarob.prado@hotmail.com>
2022-05-09 19:32:36 -0400

    refact(CoursesCarousel): adjust sizes responsivity with AspectRatio widget

--- lib/app/presentation/pages/home/widgets/home_last_courses_section.dart
+++ lib/app/presentation/pages/home/widgets/home_last_courses_section.dart
@@ -1,4 +1,5 @@
 import 'package:faeng_courses/app/presentation/common/utils/constants.dart';
+import 'package:faeng_courses/app/presentation/common/utils/responsive.dart';
 import 'package:faeng_courses/app/presentation/common/widgets/unexpected_state_widget.dart';
 import 'package:faeng_courses/app/presentation/pages/home/home_page_notifier.dart';
 import 'package:faeng_courses/app/presentation/pages/home/widgets/latest_courses_carousel.dart';
@@ -16,21 +17,23 @@ class HomeLastCoursesSection extends ConsumerWidget {
     final courses = ref.watch(getLastCoursesProvider);
     final colors = ref.watch(themeProvider).colors;
     return SliverToBoxAdapter(
-      child: Container(
-        color: colors.primaryColor,
-        padding: const EdgeInsets.symmetric(vertical: kSmallNumber),
-        height: 136,
-        child: courses.maybeWhen(
-          orElse: () => UnexpectedStateWidget(
-            iconColor: colors.white,
-            textColor: colors.white,
-            onTryAgain: () => ref.refresh(getLastCoursesProvider),
-          ),
-          loading: () => const Center(
-            child: CircularProgressIndicator(),
-          ),
-          data: (courseList) => LatestCoursesCarousel(
-            courseList: courseList,
+      child: AspectRatio(
+        aspectRatio: Responsive.isPortrait(context) ? 360 / 136 : 640 / 136,
+        child: Container(
+          color: colors.primaryColor,
+          padding: const EdgeInsets.symmetric(vertical: kSmallNumber),
+          child: courses.maybeWhen(
+            orElse: () => UnexpectedStateWidget(
+              iconColor: colors.white,
+              textColor: colors.white,
+              onTryAgain: () => ref.refresh(getLastCoursesProvider),
+            ),
+            loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+            data: (courseList) => LatestCoursesCarousel(
+              courseList: courseList,
+            ),
           ),
         ),
       ),
