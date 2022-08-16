fff92f0b8a7c4b4de830a24fcd8070cf4a728505
Alvaro Prado <alvarob.prado@hotmail.com>
2022-08-16 18:56:12 -0400

    feat(TwitterLogin): add provider to inject dependency

--- lib/core/common/providers/module_providers.dart
+++ lib/core/common/providers/module_providers.dart
@@ -1,6 +1,8 @@
+import 'package:estudaqui/core/common/env_config.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:twitter_login/twitter_login.dart';
 
 final packageInfoProvider = FutureProvider<PackageInfo>(
   (ref) async {
@@ -11,5 +13,15 @@ final packageInfoProvider = FutureProvider<PackageInfo>(
 final googleSigninProvider = Provider<GoogleSignIn>(
   (ref) {
     return GoogleSignIn(scopes: ['email', 'profile']);
+  },
+);
+
+final twitterSigninProvider = Provider<TwitterLogin>(
+  (ref) {
+    return TwitterLogin(
+      apiKey: EnvConfig.twitterApiKey,
+      apiSecretKey: EnvConfig.twitterApiSecret,
+      redirectURI: 'estudaqui://',
+    );
   },
 );
--- lib/core/common/providers/repository_providers.dart
+++ lib/core/common/providers/repository_providers.dart
@@ -8,6 +8,7 @@ import 'package:estudaqui/core/common/providers/module_providers.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:twitter_login/twitter_login.dart';
 
 final authRepositoryProvider = Provider<AuthImpRepository>(
   (ref) {
@@ -19,9 +20,14 @@ final authRepositoryProvider = Provider<AuthImpRepository>(
       googleSigninProvider,
     );
 
+    final twitterSignInProvider = ref.watch<TwitterLogin>(
+      twitterSigninProvider,
+    );
+
     return AuthImpRepository(
       authProvider: authProvider,
       googleAuthProvider: googleSignInProvider,
+      twitterAuthProvider: twitterSignInProvider,
     );
   },
 );
