ac6e9f51b2de73bd53b8b554c2490e9d2e69378b
Álvaro Prado <56163128+alvarobcprado@users.noreply.github.com>
2022-08-20 15:43:43 -0400

    Fix/adjust dotenv initialize (#44)
    * fix(EnvConfig): adjust dotenv variable name
    
    * fix: add .env file to assets

--- lib/core/common/env_config.dart
+++ lib/core/common/env_config.dart
@@ -3,5 +3,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 /// Loads the environment variables from the .env file.
 abstract class EnvConfig {
   static String twitterApiKey = dotenv.get('TWITTER_API_KEY');
-  static String twitterApiSecret = dotenv.get('TWITTER_API_SECRET');
+  static String twitterApiSecret = dotenv.get('TWITTER_API_KEY_SECRET');
 }
